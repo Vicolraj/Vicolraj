3fd08fb08abe05480638231da4843e37ee048f63
vicolraj <vicolraj@gmail.com>
2026-05-26 14:59:04 +0100

    manual sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
-      hostname: 'https://vicolraj.vercel.app',
-      dynamicRoutes: [
-        '/',
-        // add your other pages below, for example:
-        // '/about',
-        // '/projects',
-        // '/contact',
-      ],
-    }),
   ],
 })
