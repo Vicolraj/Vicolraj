6e6d013962b39d54e44dab82eaf749432fa3c45a
vicolraj <vicolraj@gmail.com>
2026-06-12 06:19:42 +0100

    getting dope

--- src/components/atom/Work.tsx
+++ src/components/atom/Work.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { PROJECTS, type ProjectItem } from '../../data';
 import './styles/work.css';
 
--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,6 +1,9 @@
 import "./styles/HeroSection.css"
 // @ts-ignore
 import Silk from "../animation/Silk/Silk"
+import SplitText from "../animation/SplitText/SplitText"
+import TextType from "../animation/TextType/TextType"
+
 import lapTop from "../../assets/img/laptop.webp"
 
 export default function HeroSection() {
@@ -14,16 +17,82 @@ export default function HeroSection() {
 
       <div className="hero-left">
         <h1 className="hero-title">
-          Victor<br />
-          <em>Raji.</em><br />
-          Fullstack<br />
-          Developer
+          <SplitText
+            text="Victor"
+            delay={50}
+            duration={1.25}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+          />
+          <br />
+          <em>
+            <SplitText
+              text="Raji."
+              delay={50}
+              duration={1.25}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+
+            />
+          </em><br />
+            <SplitText
+              text="Fullstack"
+              delay={50}
+              duration={1.45}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+            />
+          <br />
+
+          <SplitText
+              text="Developer"
+              delay={50}
+              duration={1.65}
+              ease="power3.out"
+              splitType="chars"
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              rootMargin="-100px"
+              textAlign="center"
+
+            />
+          
         </h1>
 
+
+
+
         <p className="hero-sub">
-          I build things for the web from Backends to Frontends Apps, from
-          animated components to production-grade cloud infrastructure.
-          3+ years shipping apps people actually use.
+          <TextType 
+            text={"I build things for the web from Backends to Frontends Apps, from animated components to production-grade cloud infrastructure. 3+ years shipping apps people actually use."}
+            typingSpeed={7}
+            pauseDuration={1500}
+            loop = {false}
+            showCursor
+            cursorCharacter=""
+            // texts={["Welcome to React Bits! Good to see you!","Build some amazing experiences!"]}
+            deletingSpeed={0}
+            // variableSpeedEnabled={false}
+            // variableSpeedMin={60}
+            // variableSpeedMax={120}
+            cursorBlinkDuration={0.5}
+          />
         </p>
 
         <div className="hero-actions">
